a326e5eeaae1e802b34c05a8c6cd4f24d1446bdd
AionCode <alexanderlight88@gmail.com>
2017-03-22 00:25:50 +0100

    Added back old main.h

--- main.h
+++ main.h
@@ -1,10 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_opengl.h>
-#include <SDL2/SDL_mixer.h>
-#include <SDL2/SDL_image.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_opengl.h>
+#include <SDL/SDL_mixer.h>
+#include <SDL/SDL_image.h>
 #include <iostream>
 #include <vector>
 
